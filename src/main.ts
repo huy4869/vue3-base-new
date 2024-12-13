f77075318e149fc5a4f1701527a879e136bf8697
huy4869 <tvhtranviethuy1@gmail.com>
2024-12-13 16:50:58 +0700

    element plus + tailwind

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
-import './assets/main.css'
+import './assets/scss/main.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue';
+import viVN from 'element-plus/es/locale/lang/vi';
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +15,10 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.use(ElementPlus, { zIndex: 3000, locale: viVN });
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
 
 app.mount('#app')
